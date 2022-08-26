136f9f48547f221d43fde936c5d4d4a12a87dd56
Eyael <eyaelabdissa8@@gmail.com>
2022-08-26 07:41:26 +0300

    Add string constants

---
+++ lib/presentation/constants/string_constants.dart
@@ -0,0 +1,6 @@
+class AppStrings {
+  static const String noRouteFound = "No route found";
+  static const String userName = "Username";
+
+  static const String password = "Password";
+}
--- lib/presentation/views/undefined_route_screen.dart
+++ lib/presentation/views/undefined_route_screen.dart
@@ -1,3 +1,4 @@
+import 'package:advanced_flutter/presentation/constants/string_constants.dart';
 import 'package:flutter/material.dart';
 
 class UndefinedRouteScreen extends StatelessWidget {
@@ -7,10 +8,10 @@ class UndefinedRouteScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("No route found."),
+        title: const Text(AppStrings.noRouteFound),
       ),
       body: const Center(
-        child: Text("No route found."),
+        child: Text(AppStrings.noRouteFound),
       ),
     );
   }
