360c6654166a86f1a1fd7942d4a70c1dbd247da2
Eyael <eyaelabdissa8@@gmail.com>
2022-08-26 12:17:05 +0300

    add onboarding screen constants and assets

--- lib/presentation/constants/image_values.dart
+++ lib/presentation/constants/image_values.dart
@@ -4,4 +4,9 @@ const String IMAGE_PATH = "assets/images";
 
 class ImageAssets {
   static const String LogoImage = "$IMAGE_PATH/splash_logo.png";
+
+  static const String onboardingImage1 = "$IMAGE_PATH/svg/onboarding_logo1.svg";
+  static const String onboardingImage2 = "$IMAGE_PATH/svg/onboarding_logo2.svg";
+  static const String onboardingImage3 = "$IMAGE_PATH/svg/onboarding_logo3.svg";
+  static const String onboardingImage4 = "$IMAGE_PATH/svg/onboarding_logo4.svg";
 }
